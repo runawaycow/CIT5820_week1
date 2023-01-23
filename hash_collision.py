daf86c8a3e9a69e94b8fcdbbcb38d47facb80ce4
Dan Luo <luo_dana@yahoo.com>
2023-01-24 01:14:17 +1000

    HW-try1

--- hash_collision.py
+++ hash_collision.py
@@ -1,5 +1,7 @@
 import hashlib
 import os
+import string
+import random
 
 def hash_collision(k):
     if not isinstance(k,int):
@@ -10,11 +12,22 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
    
     #Collision finding code goes here
+    bits = 1 << k
+    letters = string.ascii_letters
+    for i in range(0,2^k):    
+      string_1 =  ''.join(random.choice(letters) for i in range(k+1) )
+      hashcode_1 = hashlib.sha256(string_1.encode('utf-8')).hexdigest()
+      hashcode_bi_1 = bin(int(hashcode,16)& (bits - 1))
+
+      string_2 =  ''.join(random.choice(letters) for i in range(k+1) )  
+      hashcode_2 = hashlib.sha256(string_2.encode('utf-8')).hexdigest()
+      hashcode_bi_2 = bin(int(hashcode,16)& (bits - 1))
+      
+      if hashcode_bi_1 == hashcode_bi_2:
+        return (string_1.encode('utf-8'), string_2.encode('utf-8'))
+        #print(string_1, string_2)
+
     x = b'\x00'
     y = b'\x00'
-    
+    #print('Did not found matching')
     return( x, y )
-
-
-
-
